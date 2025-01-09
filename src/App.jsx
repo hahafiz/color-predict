a1638b642aa97954dfc188a96217b8d9f55895bb
hahafiz <hafizqamarul19@gmail.com>
2025-01-09 20:08:41 +0800

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="flex-row w-full ">
+        <div className="container mx-auto px-4 py-8 bg-slate-600 min-h-[400px]"></div>
+        <form className="container mx-auto py-8 max-w-48">
+          <label className="relative block">
+            <span className="sr-only">Guess the hex code</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+            </span>
+            <input
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Guess the hex code"
+              type="text"
+              name="hexcode"
+            />
+          </label>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
