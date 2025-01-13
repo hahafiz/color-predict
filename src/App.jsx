2cfe1d94d63b63dca4211a2d2285f46dc61a2191
hahafiz <hafizqamarul19@gmail.com>
2025-01-13 16:39:51 +0800

    apply newly learned flexbox knowledge

--- src/App.jsx
+++ src/App.jsx
@@ -80,40 +80,34 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center h-screen w-screen">
-      <Scoreboard round={round} score={score} />
-      <Timer onTimeUp={handleTimeUp} resetTrigger={resetTimer} />
-      <div className="flex justify-center items-center">
-        <div className="flex-row w-full">
-          <div
-            className="containerrounded-sm min-h-[600px]"
-            style={{ backgroundColor: backgroundColor }}
-          ></div>
-          <form
-            onSubmit={handleSubmit}
-            className="container mx-auto py-8 max-w-48"
-          >
-            <label className="relative block pb-4">
-              <span className="sr-only">Guess the hex code</span>
-              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                <svg
-                  className="h-5 w-5 fill-slate-300"
-                  viewBox="0 0 20 20"
-                ></svg>
-              </span>
-              <input
-                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                placeholder="Guess the hex code"
-                type="text"
-                name="hexcode"
-              />
-            </label>
-            <Button type="submit" className="w-full">
-              Guess
-            </Button>
-          </form>
-        </div>
+    <div
+      className="flex flex-col justify-between h-screen py-16 px-8"
+      style={{ backgroundColor: backgroundColor }}
+    >
+      <div className="flex flex-col gap-2 items-center">
+        <Scoreboard round={round} score={score} />
+        <Timer onTimeUp={handleTimeUp} resetTrigger={resetTimer} />
       </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center align-center"
+      >
+        <label className="relative block pb-2">
+          <span className="sr-only">Guess the hex code</span>
+          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+            <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+          </span>
+          <input
+            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="Guess the hex code"
+            type="text"
+            name="hexcode"
+          />
+        </label>
+        <Button type="submit" className="h-10 w-1/4">
+          Guess
+        </Button>
+      </form>
     </div>
   );
 };
--- src/components/scoreboard.jsx
+++ src/components/scoreboard.jsx
@@ -1,9 +1,9 @@
 const Scoreboard = ({ round, score }) => {
   return (
-    <>
+    <div className="flex gap-4">
       <div>Round {round}/5</div>
       <div>Score: {score}</div>
-    </>
+    </div>
   );
 };
 
