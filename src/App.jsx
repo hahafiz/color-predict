b7a72b71c761b3033101153fb19160ef675d6c71
hahafiz <hafizqamarul19@gmail.com>
2025-01-15 22:49:09 +0800

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Scoreboard from "./components/scoreboard";
 // TODO: figure out score logic -> calculate difference between hexcode vs user input -> the further the difference the lower the score
 // TODO: figure out how to put # input field
 // TODO: make the whole page background color as the random hexcode
+// TODO: add reset button
+// TODO: refactor code
 const App = () => {
   const [backgroundColor, setBackgroundColor] = useState("#000000");
   const [round, setRound] = useState(1);
